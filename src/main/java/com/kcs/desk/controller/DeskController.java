1ef9476060a615f126ac7fad61e6a54406e24408
kcs-tejaskumarvasava <62284811+kcs-tejaskumarvasava@users.noreply.github.com>
2023-02-06 17:34:51 +0530

    Update test message.

--- src/main/java/com/kcs/desk/controller/DeskController.java
+++ src/main/java/com/kcs/desk/controller/DeskController.java
@@ -43,6 +43,7 @@ public class DeskController {
 	
 	@GetMapping("/getAllDesk")
 	public  ResponseVO<List<DeskDto>> getDesk(){
+		System.out.println("test");
 		return deskService.getAllDesk();
 		
 	}
